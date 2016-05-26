b9345c1c8384c6ff0fd301658546336d5370a6d4
Alysson Vicuna <alyssontkd@gmail.com>
2016-05-25 22:51:24 -0300

    [Novo] adicionando o Arquivo de conexao de exemplo

--- config/autoload/development.exemplo.php
+++ config/autoload/development.exemplo.php
@@ -3,13 +3,8 @@
 return array(
     'db' => array(
 		'username' => 'root',
-<<<<<<< HEAD:config/autoload/development.exemplo.php
-        'password' => 'magatti123',
-        'dsn' => 'mysql:dbname=bdcatequese;host=localhost',
-=======
         'password' => '',
         'dsn' => 'mysql:dbname=bdetec;host=localhost',
->>>>>>> de1815fac8bd91feaadb9fd62e3f5d74a4a2eb1c:config/autoload/development.php
     ),
     'service_manager' => array(
         'factories' => array(
