daaa0135fbdf62ab3ff974459deb31cc12282433
Eduferr <eduardofgsantos@gmail.com>
2016-10-14 14:33:50 -0300

    Melhorando o Layout do cadastro Detalhe no Modulo TCC

--- module/Tcc/src/Tcc/Form/TccForm.php
+++ module/Tcc/src/Tcc/Form/TccForm.php
@@ -30,7 +30,7 @@ class TccForm extends AbstractForm
         $objForm->combo("id_tipo_tcc",'\TipoTcc\Service\TipoTccService', 'id', 'nm_tipo_tcc')->required(true)->label("Tipo de TCC");
         $objForm->combo("id_area_conhecimento", '\AreaConhecimento\Service\AreaConhecimentoService', 'id', 'nm_area_conhecimento')->required(true)->label("Ãrea de Conhecimento");
 
-        $objForm->combo("id_professor_orientador",'\Professor\Service\ProfessorService', 'id','nm_Professor')->required(true)->label("Nome do Professor Orientador");
+        $objForm->combo("id_professor_orientador",'\Professor\Service\ProfessorService', 'id','nm_Professor', 'retornaOrientadores')->required(true)->label("Nome do Professor Orientador");
 
         $objForm->text("tx_titulo_tcc")->required(true)->label("Título do TCC");
         $objForm->textarea("tx_resumo")->required(true)->label("Resumo");
