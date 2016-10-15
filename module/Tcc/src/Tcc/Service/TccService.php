0c19d320cc09fd5022db147f13921856e46f80ee
Alysson Vicuña de Oliveira <alyssontkd@gmail.com>
2016-10-15 11:14:20 -0300

    Adicionando metodo para Buscar TCC (Pesquisar)

--- module/Tcc/src/Tcc/Service/TccService.php
+++ module/Tcc/src/Tcc/Service/TccService.php
@@ -104,6 +104,7 @@ class TccService extends Entity
             ->join('professor', 'professor.id_professor = tcc.id_professor_orientador', ['nm_professor']);
 
         $where = [
+
         ];
 
         if (!empty($filter)) {
@@ -117,6 +118,8 @@ class TccService extends Entity
             }
         }
         $select->where($where)->order(['id_tcc DESC']);
+
+        #xd($select->getSqlString($this->getAdapter()->getPlatform()));
 
         return new Paginator(new DbSelect($select, $this->getAdapter()));
     }
@@ -192,6 +195,48 @@ class TccService extends Entity
         $select->where($where)->order(['id_concluinte DESC']);
         #xd($select->getSqlString($this->getAdapter()->getPlatform()));
         return new \Zend\Paginator\Paginator(new \Zend\Paginator\Adapter\DbSelect($select, $this->getAdapter()));
+    }
+
+    /**
+     * Realiza uma pesquisa nos trabalhos de TCC
+     *
+     * @param null $where Campos com os filtros da tela
+     * @return \Zend\Db\Adapter\Driver\ResultInterface
+     */
+    public function pesquisarTcc($where = NULL)
+    {
+
+        $sql = new Sql($this->getAdapter());
+
+        $select = $sql->select('tcc')->columns([
+            'id_tcc',
+            'id_usuario_cadastro',
+            'id_usuario_alteracao',
+            'tx_titulo_tcc',
+            'tx_resumo',
+            'dt_cadastro',
+            'dt_alteracao',
+            'nr_nota_final',
+        ])
+            // Buscando dados das Fks
+            ->join('banca_examinadora', 'banca_examinadora.id_banca_examinadora = tcc.id_banca_examinadora', ['dt_banca'])
+            ->join('area_conhecimento', 'area_conhecimento.id_area_conhecimento = tcc.id_area_conhecimento', ['nm_area_conhecimento'])
+            ->join('tipo_tcc', 'tipo_tcc.id_tipo_tcc = tcc.id_tipo_tcc', ['nm_tipo_tcc'])
+            ->join('professor', 'professor.id_professor = tcc.id_professor_orientador', ['nm_professor']);
+
+        $where = [
+            'banca_examinadora.dt_banca' => '2016-10-14 00:00:00',
+            #'banca_examinadora.dt_banca' => '2016-10-14 00:00:00',
+            #'banca_examinadora.dt_banca' => '2016-10-14 00:00:00',
+            #'banca_examinadora.dt_banca' => '2016-10-14 00:00:00',
+            #'banca_examinadora.dt_banca' => '2016-10-14 00:00:00',
+            #'banca_examinadora.dt_banca' => '2016-10-14 00:00:00',
+        ];
+
+        $select->where($where)->order(['id_tcc DESC']);
+
+        #xd($select->getSqlString($this->getAdapter()->getPlatform()));
+        return $sql->prepareStatementForSqlObject($select)->execute();
     }
 
 
